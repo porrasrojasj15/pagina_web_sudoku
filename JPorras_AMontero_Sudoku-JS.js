9eb4d8910b246a366c6e5e17a132b9fe951dbe6d
Jorge Isaac Porras Rojas <porrasrojasj15@gmail.com>
2018-07-10 13:46:38 -0600

    Añadidos los sudokus, botones de revisar, y función para desplegar

---
+++ JPorras_AMontero_Sudoku-JS.js
@@ -0,0 +1,21 @@
+function desplegar() {
+  var sudokus = document.getElementsByClassName("sudoku");
+  var i;
+  for (i = 0; i < sudokus.length; i++) {
+      sudokus[i].hidden = false;
+    }
+  /*getElementsByClassName retorna una lista con todos los elementos de clase
+  sudoku, que se encontraban ocultos, esta función recorre esa lista y los muestra*/
+}
+
+function validar() {
+  t
+}
+
+function alocar(){
+
+}
+
+function revisar(){
+
+}
